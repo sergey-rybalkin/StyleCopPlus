4e235f337e2bec3e8de9c517f6535dfd4ce4865a
Sergey Rybalkin <rybalkinsp@gmail.com>
2016-07-06 22:37:50 +0300

    Version number update, PowerShell build script

---
+++ build.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+StyleCopPlus analyzers build script.
+
+.DESCRIPTION
+Builds specified configuration of StyleCopPlus analyzers and optionally installs nuget package to the local feed.
+
+#>
+[CmdletBinding()]
+PARAM (
+    [string]$Configuration = "Release",
+
+    [switch]$Install = $true
+)
+
+# Ensure that we have MSBuild logs folder
+if (-Not (Test-Path logs)) {
+    mkdir logs
+}
+
+$MSBuildCmd = "${env:ProgramFiles(x86)}\MSBuild\14.0\Bin\MSBuild.exe"
+& $MSBuildCmd "StyleCopPlus.Analyzers.sln" /m /nr:false /t:Build "/p:Configuration=$Configuration" /v:M /fl "/flp:LogFile=logs\msbuild.log;Verbosity=Normal"
+
+if ($LASTEXITCODE -eq 0 -and $Install) {
+    $Package = Get-ChildItem -Recurse -Path "src\StyleCopPlus.Analyzers\bin\Release\" -Filter "*.nupkg" | select -last 1
+    & "NuGet.exe" add $Package.FullName -source "D:\dev\_nuget"
+}
